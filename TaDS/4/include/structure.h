e256b011bee13c21c275ad74c0e3520d036dba81
Watcher <ivndonat@yandex.ru>
2022-02-15 06:25:38 +0300

    init 4

---
+++ TaDS/4/include/structure.h
@@ -0,0 +1,16 @@
+#ifndef __STRUCTURE__
+#define __STRUCTURE__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <time.h>
+
+#define ERROR_NONE 0
+#define ERROR_INPUT 1
+#define ERROR_CALC 2
+#define ERROR_MEMORY 3
+
+
+#endif
---
+++ TaDS/4/src/main.c
@@ -0,0 +1,9 @@
+#include "../include/structure.h"
+
+
+int main()
+{
+
+
+    return ERROR_NONE;
+}
